0528a96aef53c87786a487b0b69fe5335f85ebcc
anastasia <anyasirman@outlook.com>
2023-01-17 20:33:51 -0500

    set up sequelize and added a category model as well as two routes

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express')
+const { sequelize, Category } = require('./models')
+const category = require('./models/category')
 const app = express()
+app.use(express.json())
 
 
+app.get('/categories', async(req, res)=> {
+    try{
+        let categories = await Category.findAll()
+        return res.json(categories)
+    }catch(err){
+        console.log(err)
+        return res.status(500)
+        // res.send('hi')
+    }
+})
 
-app.get('/', (req, res)=> {
-    res.send('hi')
+app.post('/categories', async(req,res) => {
+    const {name, description} = req.body
+    try{
+        let category = await Category.create({name, description})
+        return res.json(category)
+    }catch(err){
+        console.log(err)
+        return res.json(err)
+    }
 })
 
 
 
-
-
-
-
-
-app.listen(3000)
+app.listen({port: 3000}, async() => {
+    console.log('server is up')
+    await sequelize.authenticate()
+    console.log('database connected')
+})
