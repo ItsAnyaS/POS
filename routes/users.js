3a76621df881bfbf48f8b02e483951dc5914b1f8
anastasia <anyasirman@outlook.com>
2023-01-22 17:41:15 -0500

    added uuid lookup for users and removed id showing in api requesets

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const {User} = require('../models')
+const { User } = require('../models')
 
 router.get('/', async(req,res)=> {
 let users = await User.findAll()
 return res.json(users)
+})
+
+router.get('/:id', async(req,res)=> {
+    let user = await User.findOne({where: {uuid: req.params.id}})
+    return res.json(user)
 })
 
 router.post('/', async(req, res)=> {
