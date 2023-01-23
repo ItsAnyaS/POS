3718820f59cd7ab374b89b1fc6bcb81c919a0e03
anastasia <anyasirman@outlook.com>
2023-01-23 16:25:34 -0500

    added session id to transaction table

--- models/session.js
+++ models/session.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, Transaction
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Session extends Model {
@@ -9,9 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User}) {
+    static associate({User, Transaction}) {
       // define association here
       this.belongsTo(User, {foreignKey: 'userId'})
+      this.hasMany(Transaction, {foreignKey: 'sessionId'})
     }
   }
   Session.init({
