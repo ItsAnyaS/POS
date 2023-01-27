e7cb45c25a81b7e12c96ba6950d365aaecebba7e
anastasia <anyasirman@outlook.com>
2023-01-26 19:06:44 -0500

    added bcyrpt to encrypt userpasswords

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const jwt = require('jsonwebtoken');
 const router = express.Router()
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const { User } = require('../models')
+
+
+//* Create token with 24 expiry
+const signToken = (user) => {
+    return jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data: user.uuid}, process.env.JWT_SECRET_KEY);
+}
+
+const decodeToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET_KEY)
+}
+
+// const encryptPassword =(hash) => {
+//     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+//         // Store hash in your password DB.
+//         console.log(hash)
+//     });
+// }
 
 //* Sign in
 
@@ -12,8 +31,9 @@ router.post('/login', async(req,res)=> {
     try{
         let user = await  User.findOne({where: {username}})
         if (user){
-            if (user.password === password){
-                let token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data: user.uuid}, process.env.JWT_SECRET_KEY);
+            let decryptedPassword = await bcrypt.compare(password, user.password)
+            if (decryptedPassword){
+                let token = signToken(user)
                 return res.json({
                     authToken: token,
                     country: user.country,
@@ -36,9 +56,8 @@ router.post('/login', async(req,res)=> {
 
 router.get('/:token', async(req, res)=> {
     let token = req.params.token
-    // console.log(process.env.JWT_SECRET_KEY)
     try{
-        let decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        let decodedToken = decodeToken(token)
         if (!decodedToken){
             return res.json({message: "Not valid user"})
         }
@@ -55,12 +74,24 @@ router.get('/:token', async(req, res)=> {
     }
 })
 
-// router.get('/:uuid', async(req, res)=> {
-//     token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data: 'foo'}, 'secret');
-//     let decoded = jwt.verify(token, 'secret')
-//     console.log(decoded)
-// return res.json(token)
-// })
+
+//* Create user / sign up
+router.post('/signup', async(req,res) => {
+    let { username, password, country, state} = req.body
+    try {
+        let userExists = await User.findOne({where: {username}})
+        if (!userExists){
+        let encryptedPassword = await bcrypt.hash(password, 10)
+        let user = await User.create({username, password: encryptedPassword, country, state})
+        return res.json(user)
+        }else {
+            return res.json({message: "A user with that name already exists"})
+        }
+    }catch(err){
+        console.log(err)
+        return res.json(err)
+    }
+})
 
 
 
