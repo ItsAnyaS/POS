03a116fa1706fcd01f9b83c30704f1bffd2eb8fc
anastasia <anyasirman@outlook.com>
2023-01-30 17:33:17 -0500

    changed actual module

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,32 +3,30 @@ import { GrTransaction } from 'react-icons/gr'
 import {AiOutlineHome } from 'react-icons/ai'
 import { GiHamburgerMenu} from 'react-icons/gi'
 import { NavLink } from "react-router-dom"
-import Cookies from 'js-cookie'
 import { useEffect, useState } from "react"
 
 const Navbar = () => {
 
 const [usable, setUsable] = useState<boolean>(true)
 
-const validateUser = async() => {
-let user = Cookies.get('auth-token')
-      if (user){
-            let req = await fetch(`/auth/${user}`)
-            let res = await req.json()
-            if (res.message === 'valid user'){
-            // console.log('logged in')
-            setUsable(true)
-            }else {
-            // console.log('not logged in')
-            setUsable(false)
-            }
-      }else {
-            setUsable(false)
-      }
-}
+// const validateUser = async() => {
+// let user = Cookies.get('auth-token')
+//       if (user){
+//             let req = await fetch(`/auth/${user}`)
+//             let res = await req.json()
+//             if (res.message === 'valid user'){
+//             // console.log('logged in')
+//             setUsable(true)
+//             }else {
+//             // console.log('not logged in')
+//             setUsable(false)
+//             }
+//       }else {
+//             setUsable(false)
+//       }
+// }
 
 useEffect(()=> {
-      validateUser()
 },[])
       
 return ( 
